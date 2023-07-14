7ede28237cb74fcc5765d3ea69cbd9b2cf8ee33c
OleksandraMahera <126208886+OleksandraMahera@users.noreply.github.com>
2023-07-15 00:46:29 +0200

    js-hw-06

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,5 +1,5 @@
 const fontSizeControl = document.getElementById('font-size-control');
-    const textElement = document.getElementById('text');
+const textElement = document.getElementById('text');
 
     fontSizeControl.addEventListener('input', function() {
       const fontSize = this.value + 'px';
